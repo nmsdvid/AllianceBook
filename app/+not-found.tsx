a53bd6a8c5f5ee5a65736ef66c9e05748a078907
David Nemes <nmsdvid@gmail.com>
2025-03-04 09:08:53 +0100

    ➕ Added prettier and eslint

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,13 +1,11 @@
-import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { Stack } from "expo-router";
+import { View } from "react-native";
 
 export default function NotFoundScreen() {
-    return (
-        <>
-            <Stack.Screen options={{ title: 'Oops!' }} />
-            <View>
-                Oops! Millennium Falcon not found!
-            </View>
-        </>
-    );
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View>Oops! Millennium Falcon not found!</View>
+    </>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,15 @@
-import { DarkTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-
   return (
-      <ThemeProvider value={DarkTheme}>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+    <ThemeProvider value={DarkTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
