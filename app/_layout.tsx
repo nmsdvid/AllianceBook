80ebc27474a3abb9e2f8a75a7572bcf67da40653
David Nemes <nmsdvid@gmail.com>
2025-03-04 08:59:39 +0100

    ➕ Added expo-router + setup file based routing

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+
+  return (
+      <ThemeProvider value={DarkTheme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+  );
+}
